81ff711d276a5e2cbea85286a9a2caee4895f29b
Ana-Maria Marcu <anamaria.marcu@gmail.com>
2016-02-04 17:01:01 +0100

    early implementation of http://anamaria-tosh:8080/browse/SP-3

---
+++ try-out/src/main/java/com/btc/example/Feature.java
@@ -0,0 +1,14 @@
+package com.btc.example;
+
+/**
+ * Created by Ana Maria on 04.02.2016.
+ */
+public class Feature {
+    public boolean isAvailable(){
+        return false;
+    }
+
+    public void makeAvailable(){
+        //some code here
+    }
+}
