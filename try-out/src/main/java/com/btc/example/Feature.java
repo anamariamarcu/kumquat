1e68512109ccef43162b688e07daf6c435458ccb
Ana-Maria Marcu <anamaria.marcu@gmail.com>
2016-02-05 09:26:31 +0100

    needed for SP-4

--- try-out/src/main/java/com/btc/example/Feature.java
+++ try-out/src/main/java/com/btc/example/Feature.java
@@ -4,11 +4,13 @@ package com.btc.example;
  * Created by Ana Maria on 04.02.2016.
  */
 public class Feature {
-    public boolean isAvailable(){
-        return false;
+    private boolean available;
+
+    public boolean isAvailable() {
+        return available;
     }
 
-    public void makeAvailable(){
-        //some code here
+    public void makeAvailable() {
+        available = true;
     }
 }
